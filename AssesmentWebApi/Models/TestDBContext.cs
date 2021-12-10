f70027c3d210c6c0db3daa39b14959cc022c6712
Phemelo <phemelopula@gmail.com>
2021-12-10 15:19:10 +0200

    Second Commit.

--- AssesmentWebApi/Models/TestDBContext.cs
+++ AssesmentWebApi/Models/TestDBContext.cs
@@ -19,16 +19,7 @@ namespace AssesmentWebApi.Models
 
         public virtual DbSet<Coin> Coins { get; set; }
         public virtual DbSet<CoinType> CoinTypes { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=197.242.149.132;Database=TestDB;MultipleActiveResultSets=true;User ID=sa;Password=5un5t0n3");
-            }
-        }
-
+              
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Assessment-
 Assessment 
+
+How To Run Project. 
+
+1. Find DB Script inside the project /AssesmentWebApi/DB Script.sql to create the DB
+
+2. Edit connection string in Appsettings.json to point to your DB
+
+3. Insert data into cointypes for coin validation with the data below
+
+Denomination	Value	Weight	
+Penny	        $0.01  (0.088 oz)	 
+Nickel	        $0.05  (0.176 oz)	 
+Dime	        $0.10  (0.08 oz)	  
+Quarter	        $0.25  (0.2 oz)	  
+Half Dollar	    $0.50  (0.40 oz)   
+Dollar Coin	    $1.00  (0.286 oz)	
